4676e61bbc2eb815ab7c8cf99a9f5db2fdfed2f6
tokoyax <tokoyax.dev@gmail.com>
2017-11-18 22:04:05 +0900

    Add description

--- mix.exs
+++ mix.exs
@@ -1,11 +1,18 @@
 defmodule No.Mixfile do
   use Mix.Project
 
+  @description"""
+  This is `no` command that outputs `n` implemented in Elixir.
+  """
+
   def project do
     [
       app: :no,
       version: "0.1.0",
       elixir: "~> 1.5",
+      name: "No",
+      description: @description,
+      package: package(),
       escript: escript_config(),
       start_permanent: Mix.env == :prod,
       deps: deps()
@@ -19,13 +26,24 @@ defmodule No.Mixfile do
     ]
   end
 
+  def package do
+    [
+      maintainers: ["tokoyax"],
+      licenses: ["MIT"],
+      links: %{ "Github": "https://github.com/tokoyax/no" }
+    ]
+  end
+
   defp escript_config do
-    [main_module: No]
+    [
+      main_module: No
+    ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
